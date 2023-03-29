399ba4c47b7c2c45a9e681ee6b679b2fb81da951
jt914 <josephtso914@gmail.com>
2023-03-29 13:02:23 -0400

    wasd

--- main.js
+++ main.js
@@ -26,7 +26,65 @@ function imageToBase64(img) {
     });
 }
 
+const fileInput = document.getElementById('fileInput');
+const submitBtn = document.getElementById('submitBtn');
+const plantName = document.getElementById('plantName');
+const commonNames = document.getElementById('commonNames');
+const wikiUrl = document.getElementById('wikiUrl');
+const wikiDescription = document.getElementById('wikiDescription');
+const preview = document.getElementById('preview');
+const loading = document.getElementById('loading');
+const result = document.getElementById('result');
 
+function imageToBase64(img) {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            preview.src = reader.result;
+            resolve(reader.result.split(',')[1]);
+        };
+        reader.readAsDataURL(img);
+    });
+}
+
+async function identifyPlant(base64Image) {
+    const response = await fetch('https://solvefortomorrow.herokuapp.com/identify', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ imageBase64: base64Image }),
+    });
+
+    const data = await response.json();
+    return data.suggestions[0];
+}
+
+fileInput.addEventListener('change', (e) => {
+    if (e.target.files && e.target.files[0]) {
+        imageToBase64(e.target.files[0]);
+    }
+});
+
+submitBtn.addEventListener('click', async () => {
+    if (!fileInput.files[0]) {
+        alert('Please upload an image');
+        return;
+    }
+
+    submitBtn.disabled = true;
+    loading.style.display = 'block';
+
+    const base64Image = await imageToBase64(fileInput.files[0]);
+    const plantData = await identifyPlant(base64Image);
+
+    plantName.textContent = plantData.plant_name;
+    commonNames.textContent = `Common names: ${plantData.plant_details.common_names.join(', ')}`;
+    wikiUrl.href = plantData.plant_details.url;
+    wikiDescription.textContent = plantData.plant_details.wiki_description.value;
+
+    result.style.display = 'block';
+    loading.style.display = 'none';
+    submitBtn.disabled = false;
+});
 
 function createInvasiveMessage(plantName, invasiveStatus) {
     let invasiveMessage = '';
